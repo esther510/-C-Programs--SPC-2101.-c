8c858d9060875f1e172970d5814369716b2d7aa5
esther510 <odongoesther017@gmail.com>
2025-09-24 23:27:58 +0300

    Add files via upload

---
+++ software engineering units.c
@@ -0,0 +1,19 @@
+//HELLO WORLD
+/*
+NAME: Odongo Esther 
+REG NO: PA106/G/28753/25
+DESCRIPTION:PROGRAMME TO DISPLAY ALL UNITS OF THIS SEMISTER
+*/
+#include <stdint.h>preprcessor directive:scanf(),
+//main function
+int main()
+{
+	printf("Descrete mathematics \n");
+	printf("Mathematics for science \n");
+	printf("Fundamentals of ICT\n");
+	printf("Computer Systems ans Organisation\n");
+	printf("Communication Skills\n");
+	printf("Introduction to Programming & Problem Solving\n");
+	printf("Fundamentals of System Analysis & Design\n");
+	return 0;
+}
