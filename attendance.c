b3beeb92ff9266750678a14867990801b2735990
esther510 <odongoesther017@gmail.com>
2025-10-01 20:09:44 +0300

    Add files via upload

---
+++ attendance.c
@@ -0,0 +1,23 @@
+//HELLO WORLD
+/*
+name:Odongo Esther were
+Reg:PA106/G/28753/25
+Date:30/09/2025
+*/
+
+#include<stdio.h>
+int main(){
+    int  attendance,marks;
+    printf("\nENTER YOUR ATTENDANCE: ");
+    scanf("%d",&attendance);
+    printf("\nENTER YOUR MARKS: ");
+    scanf("%d",&marks);
+    if(attendance>=75&&marks>=40){
+     printf("you are eligible for final exams");
+     }
+     else
+     {
+     printf("you are not eligible for final exams");
+     }
+     return 0;
+ }    
